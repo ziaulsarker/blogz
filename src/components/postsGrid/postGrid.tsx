f981d461356731280d522b50abc0a7f423652d18
ziaul sarker <ziaulsarker@gmail.com>
2024-02-26 23:43:11 -0500

    updates

--- src/components/postsGrid/postGrid.tsx
+++ src/components/postsGrid/postGrid.tsx
@@ -1,10 +1,33 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from "next/link";
 
 export default function PostGrid({ posts } = { posts: [] }) {
-  return posts.map((post: { data: { title: string }; content: string }) => (
-    <div key={post.data.title}>
-      <h1>{post.data.title}</h1>
-      <MDXRemote key={post.data.title} source={post.content} />
-    </div>
-  ));
+  return posts.map(
+    (post: {
+      data: {
+        title: string;
+        description: string;
+        published: string;
+        slug: string;
+      };
+      content: string;
+    }) => (
+      <Link
+        key={post.data.title}
+        href={post.data.slug}
+        style={{
+          textDecoration: "none",
+          color: "inherit",
+        }}
+      >
+        <div>
+          <h2>{post.data.title}</h2>
+          <div>
+            <span>{new Date(post.data.published).toLocaleDateString()}</span>
+          </div>
+          <p>{post.data.description}</p>
+        </div>
+      </Link>
+    )
+  );
 }
