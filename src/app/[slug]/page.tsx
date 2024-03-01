60a675b597693db14bc47f39ef2c6d76725efeee
ziaul sarker <ziaulsarker@gmail.com>
2024-02-29 19:01:49 -0500

    updates

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { usePost } from "../../hooks/usePosts";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import styles from "./(styles)/page.module.scss";
+import { Suspense } from "react";
 
 export default async function RemoteMdxPage({
   params: { slug },
@@ -8,21 +13,25 @@ export default async function RemoteMdxPage({
 }) {
   const postData = await usePost(slug);
 
+  console.log({ postData });
+
   return (
-    <MDXRemote
-      source={`
-  # Welcome to my MDX page!
- 
-  This is some **bold** and _italics_ text.
-  
-  This is a list in markdown:
-  
-  - One
-  - Two
-  - Three
-  
-  Checkout my React component: 
-  `}
-    />
+    <div>
+      <article>
+        <h1>{postData.data.title}</h1>
+        <MDXRemote source={postData.content} />
+      </article>
+
+      <div>
+        <Link href="/" className={styles.back}>
+          <span>
+            <Suspense fallback={<p>Loading...</p>}>
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </Suspense>
+          </span>
+          home
+        </Link>
+      </div>
+    </div>
   );
 }
