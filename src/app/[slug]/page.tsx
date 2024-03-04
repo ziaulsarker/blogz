6562beedcd5c0c537dba0e83b55a4e2a23e01214
ziaul sarker <ziaulsarker@gmail.com>
2024-03-03 19:16:43 -0500

    some updates

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,10 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { usePost } from "../../hooks/usePosts";
 import Link from "next/link";
+import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import styles from "./(styles)/page.module.scss";
-import { Suspense } from "react";
 
 export default async function RemoteMdxPage({
   params: { slug },
@@ -18,6 +18,15 @@ export default async function RemoteMdxPage({
   return (
     <div>
       <article>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "300px",
+          }}
+        >
+          <Image fill alt={postData.data.title} src="/maps-in-js.jpeg" />
+        </div>
         <h1>{postData.data.title}</h1>
         <MDXRemote source={postData.content} />
       </article>
