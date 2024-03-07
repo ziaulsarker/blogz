1532447e0b2029e7c6f7dd124816f908e8567be6
ziaul sarker <ziaulsarker@gmail.com>
2024-03-07 00:58:57 -0500

    updtes

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -16,20 +16,20 @@ const options = {
 
 const componentsMapper: MDXComponents | MergeComponents | null | undefined = {
   p: ({ children }: { children?: ReactNode }) => (
-    <p className="text-sm md:text-base">{children}</p>
+    <p className="text-sm md:text-base mb-4">{children}</p>
   ),
   Span: ({ children }: { children?: ReactNode }) => (
-    <span className="block text-xs md:text-sm mt-4 mb-2">{children}</span>
+    <span className="block text-xs md:text-sm mb-2">{children}</span>
   ),
 
   ul: ({ children }: { children?: ReactNode }) => (
-    <ul className="p-4"> {children} </ul>
+    <ul className="mx-6 mb-4"> {children} </ul>
   ),
   li: ({ children }: { children?: ReactNode }) => (
     <li className="list-disc mb-2 text-sm"> {children} </li>
   ),
   code: ({ children }: { children?: ReactNode }) => (
-    <code className="text-wrap p-2 px-6">
+    <code className="bg-[#282a36] text-white p-6 md:p-8 mb-4 block">
       <pre>{children}</pre>
     </code>
   ),
