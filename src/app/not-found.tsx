e86bc5fe166d87dd3eb1b34dcfcfcd3de82d9c50
ziaul sarker <ziaulsarker@gmail.com>
2024-03-14 00:31:25 -0400

    updates

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,23 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function NotFound() {
   const path = usePathname();
 
-  return <h1>404 - {path} Not Found! </h1>;
+  return (
+    <div>
+      <h1>404 - {path} Not Found! </h1>
+      <div className="my-3 flex">
+        back to
+        <Link
+          href={`/?category=all`}
+          className="ml-2 px-3 py-1 inline-flex text-xs rounded-full bg-[#49c5b6] hover:bg-[#222] text-white  dark:bg-[#e7b10a] dark:hover:bg-[#fff] dark:hover:text-[#222] transition-all duration-200 ease-in-out font-bold"
+        >
+          all posts
+        </Link>
+      </div>
+    </div>
+  );
 }
