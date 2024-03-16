fda5fe7cc051121d9c0714bade3d0304e7ebeab2
ziaul sarker <ziaulsarker@gmail.com>
2024-03-15 23:00:41 -0400

    update

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,13 +1,13 @@
-import { checkEnvironment } from "src/utils";
+import { BASE_URL } from "src/utils";
 
 export async function usePost(slug: string) {
-  const url = `${checkEnvironment()}/api/posts/${slug}`;
+  const url = `${BASE_URL}/api/posts/${slug}`;
   const res = await fetch(url);
   return !res.ok ? [] : res.json();
 }
 
 export async function usePosts() {
-  const url = `${checkEnvironment()}/api/posts`;
+  const url = `${BASE_URL}/api/posts`;
   const res = await fetch(url);
   return !res.ok ? [] : res.json();
 }
