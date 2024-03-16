12a2e4f91ab30ed376dde504bdfa56fca9c3c40f
ziaul sarker <ziaulsarker@gmail.com>
2024-03-15 22:03:12 -0400

    update

--- src/components/postsGrid/postGrid.tsx
+++ src/components/postsGrid/postGrid.tsx
@@ -2,33 +2,37 @@ import Link from "next/link";
 import styles from "./postGrid.module.scss";
 
 export default function PostGrid({ posts } = { posts: [] }) {
-  return posts.map(
-    (post: {
-      data: {
-        title: string;
-        description: string;
-        published: string;
-        slug: string;
-      };
-      content: string;
-    }) => (
-      <Link
-        key={post.data.title}
-        href={post.data.slug}
-        style={{
-          textDecoration: "none",
-          color: "inherit",
-          display: "block",
-        }}
-      >
-        <article className={styles.postGrid}>
-          <div className={styles.published}>
-            <span>{new Date(post.data.published).toDateString()}</span>
-          </div>
-          <h2 className={styles.title}>{post.data.title}</h2>
-          <p className={styles.description}>{post.data.description}</p>
-        </article>
-      </Link>
+  return posts.length > 0 ? (
+    posts.map(
+      (post: {
+        data: {
+          title: string;
+          description: string;
+          published: string;
+          slug: string;
+        };
+        content: string;
+      }) => (
+        <Link
+          key={post.data.title}
+          href={post.data.slug}
+          style={{
+            textDecoration: "none",
+            color: "inherit",
+            display: "block",
+          }}
+        >
+          <article className={styles.postGrid}>
+            <div className={styles.published}>
+              <span>{new Date(post.data.published).toDateString()}</span>
+            </div>
+            <h2 className={styles.title}>{post.data.title}</h2>
+            <p className={styles.description}>{post.data.description}</p>
+          </article>
+        </Link>
+      )
     )
+  ) : (
+    <h2>No Posts Found</h2>
   );
 }
