93d9eb18ca9f6fbbb7e7004940a3483cd532b547
ziaul sarker <ziaulsarker@gmail.com>
2026-02-08 17:20:47 -0500

    update to next 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import  { type NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const response = NextResponse.next()
   return response;
 }
